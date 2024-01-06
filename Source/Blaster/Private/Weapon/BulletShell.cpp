5ab9537a7eef8720a7358cb14b77c2f84f384d38
Evernight33 <il.radkev@gmail.com>
2024-01-06 13:07:00 +0200

    Added physics to bullet shells

--- Source/Blaster/Private/Weapon/BulletShell.cpp
+++ Source/Blaster/Private/Weapon/BulletShell.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Weapon/BulletShell.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
 
 ABulletShell::ABulletShell()
 {
@@ -9,11 +11,29 @@ ABulletShell::ABulletShell()
 
 	BulletShell = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BulletShell"));
 	SetRootComponent(BulletShell);
+
+	BulletShell->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+
+	BulletShell->SetSimulatePhysics(true);
+	BulletShell->SetEnableGravity(true);
+	BulletShell->SetSimulatePhysics(true);
+	BulletShell->SetNotifyRigidBodyCollision(true);
 }
 
 void ABulletShell::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	BulletShell->OnComponentHit.AddDynamic(this, &ABulletShell::OnHit);
+	BulletShell->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+
+	SetLifeSpan(2.5);
 }
 
+void ABulletShell::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (ShellSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+}
--- Source/Blaster/Public/Weapon/BulletShell.h
+++ Source/Blaster/Public/Weapon/BulletShell.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "BulletShell.generated.h"
 
+class USoundCue;
+
 UCLASS()
 class BLASTER_API ABulletShell : public AActor
 {
@@ -18,8 +20,16 @@ private:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* BulletShell;
 
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpulse = 10.f;
+
+	UPROPERTY(EditAnywhere)
+	USoundCue* ShellSound;
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 public:	
 
