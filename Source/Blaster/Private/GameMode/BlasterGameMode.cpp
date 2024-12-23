d711efdedc09c41cdfc86dcc042fa275d2e2b140
Evernight33 <il.radkev@gmail.com>
2024-12-23 20:29:10 +0200

    Added match state delay time

--- Source/Blaster/Private/GameMode/BlasterGameMode.cpp
+++ Source/Blaster/Private/GameMode/BlasterGameMode.cpp
@@ -8,6 +8,33 @@
 #include "GameFramework/PlayerStart.h"
 #include "Blaster/Public/PlayerState/BlasterPlayerState.h"
 
+ABlasterGameMode::ABlasterGameMode()
+{
+	bDelayedStart = true;
+}
+
+void ABlasterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LevelStartingTime = GetWorld()->GetTimeSeconds();
+}
+
+void ABlasterGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (MatchState == MatchState::WaitingToStart)
+	{
+		CountDownTime = WarmupTime - GetWorld()->GetTimeSeconds() + LevelStartingTime;
+
+		if (CountDownTime <= 0.0f)
+		{
+			StartMatch();
+		}
+	}
+}
+
 void ABlasterGameMode::PlayerEliminated(ABlasterCharacter* ElimmedCharacter, ABlasterPlayerController* VictimController, ABlasterPlayerController* AttackerController)
 {
 	ABlasterPlayerState* AttackerPlayerState = AttackerController ? Cast<ABlasterPlayerState>(AttackerController->PlayerState) : nullptr;
--- Source/Blaster/Public/GameMode/BlasterGameMode.h
+++ Source/Blaster/Public/GameMode/BlasterGameMode.h
@@ -15,6 +15,20 @@ class BLASTER_API ABlasterGameMode : public AGameMode
 {
 	GENERATED_BODY()
 public:
+	ABlasterGameMode();
+
+	virtual void Tick(float DeltaTime) override;
 	virtual void PlayerEliminated(ABlasterCharacter* ElimmedCharacter, ABlasterPlayerController* VictimController, ABlasterPlayerController* AttackerController);
 	virtual void RequestRespawn(ACharacter* EliminatedCharacter, AController* EliminatedController);
+
+	UPROPERTY(EditDefaultsOnly)
+	float WarmupTime = 3.0f;
+
+	float LevelStartingTime = 0.0f;
+
+private:
+	float CountDownTime = 0.0f;
+
+protected:
+	virtual void BeginPlay() override;
 };
