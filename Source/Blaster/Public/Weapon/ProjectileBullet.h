b9fcef51071830e9b3f076c15c274b413ae0425e
Evernight33 <il.radkev@gmail.com>
2026-02-07 23:22:19 +0200

    Added projectile path prediction

--- Source/Blaster/Public/Weapon/ProjectileBullet.h
+++ Source/Blaster/Public/Weapon/ProjectileBullet.h
@@ -14,6 +14,18 @@ class BLASTER_API AProjectileBullet : public AProjectile
 public:
 	AProjectileBullet();
 
+	/*
+	* Used with server side rewind
+	*/
+
+	bool bUseServerSideRewind = false;
+	FVector_NetQuantize TraceStart;
+	FVector_NetQuantize100 InitialVelocity;
+
+	UPROPERTY(EditAnywhere)
+	float InitialSpeed = 15000.f;
+
 protected:
 	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
+	virtual void BeginPlay() override;
 };
